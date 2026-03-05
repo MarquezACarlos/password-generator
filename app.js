07375612b8060b8f90885336d1597160bdaab51f
iMrRouse <enderlord1873@gmail.com>
2026-03-05 13:28:16 -0600

    pwupdates

--- app.js
+++ app.js
@@ -14,42 +14,64 @@ Optional Features/Bonus:
  -Retrieve Passwords
 */
 
-
+// for (const num of pwSeed){
+      // const set = num%4;
+      // if(set == 0){
+      //   password += charSet[num%26];
+      //   document.getElementById("passwordOut").value = password;
+      // }
+      // else if (set == 1){
+      //   password += numSet[num%10];
+      //   document.getElementById("passwordOut").value = password;
+      // }
+      // else if (set == 2){
+      //   password += specialSet[num%22];
+      //   document.getElementById("passwordOut").value = password;
+      // }
+      // else{
+      //   password += charSet[num%26].toUpperCase();
+      //   document.getElementById("passwordOut").value = password;
+      // }
 // var upper = document.getElementById("optUpper").checked;
 //   if (upper){
 //     alert("yes");
 //   }
 //   alert(document.getElementById("optUpper").value);
 
-const charSet = ["a", "b","c", "d","e", "f","g", "h","i", "j","k", "l","m", "n","o", "p","q", "r","s", "t","u", "v","w", "x","y", "z"];
+const lowerSet = ["a", "b","c", "d","e", "f","g", "h","i", "j","k", "l","m", "n","o", "p","q", "r","s", "t","u", "v","w", "x","y", "z"];
+const upperSet = ["A", "B","C", "D","E", "F","G", "H","I", "J","K", "L","M", "N","O", "P","Q", "R","S", "T","U", "V","W", "X","Y", "Z"];
 const numSet = ["0","1","2","3","4","5","6","7","8","9"];
 const specialSet = ["!","@","#","$","%","^","&","*","+","=", "/", "|", "\\", ";", ":", "?", "\"", "\'", ",", ".", "~", "`"]; //22
+const minusSet = ["-"];
+const underSet = ["_"];
+const spaceSet = [" "];
+const bracketSet = ["[", "]", "{", "}", "(", ")"];
+
 
 function generatePassword(){
+  const masterSet = [];
   var password = "";
   const pwLen = document.getElementById("length").value;
- 
+  if(document.getElementById("optUpper").checked) masterSet.push(upperSet)
+  if(document.getElementById("optLower").checked) masterSet.push(lowerSet)
+  if(document.getElementById("optDigits").checked) masterSet.push(numSet)
+  if(document.getElementById("optMinus").checked) masterSet.push(minusSet)
+  if(document.getElementById("optUnderline").checked) masterSet.push(underSet)
+  if(document.getElementById("optSpace").checked) masterSet.push(spaceSet)
+  if(document.getElementById("optSpecial").checked) masterSet.push(specialSet)
+  if(document.getElementById("optBrackets").checked) masterSet.push(bracketSet)
+
+
   const pwSeed = new Uint32Array(pwLen);
   self.crypto.getRandomValues(pwSeed);
-  for (const num of pwSeed){
-      const set = num%4;
-      if(set == 0){
-        password += charSet[num%26];
-        document.getElementById("passwordOut").value = password;
-      }
-      else if (set == 1){
-        password += numSet[num%10];
-        document.getElementById("passwordOut").value = password;
-      }
-      else if (set == 2){
-        password += specialSet[num%22];
-        document.getElementById("passwordOut").value = password;
-      }
-      else{
-        password += charSet[num%26].toUpperCase();
-        document.getElementById("passwordOut").value = password;
-      }
+  
+  for(const num of pwSeed){
+    const i = num%masterSet.length;
+    const j = num%masterSet[i].length;
+    password += masterSet[i][j];
+    document.getElementById("passwordOut").value = password;
   }
+  
   
   
   
