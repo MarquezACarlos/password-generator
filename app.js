574694917abb48363b406a75af129be77e65ec5e
camarquez <camarquez@crimson.ua.edu>
2026-03-05 09:45:19 -0600

    Added Extra Requirements

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+/*
+Requirements:
+ -Copy button
+ -Save Encrypted
+ -Save username
+ -Save Identifier (Google.com, email, etc.)
+
+Optional Features/Bonus:
+ -Retrieve Passwords
+ -Option to store payment information:
+    -Card Number & Length (Double check for each type of credit card, and VALID NUMBERS)
+    -Expiration Date (Won't store if expired)
+    -CVV (PIN Type, some have 3 others have 4)
+ -Retrieve Passwords
+*/
+
 document.addEventListener("DOMContentLoaded", () => {
   const lengthInput = document.getElementById("length");
   const lengthHint = document.getElementById("lengthHint");
